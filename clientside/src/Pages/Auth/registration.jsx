836ac63a17174347d38f915e9988aa659eed261a
Pranjal Maurya <140903582+PranjalMaurya07@users.noreply.github.com>
2024-11-19 04:37:35 +0530

    Update registration.jsx

--- clientside/src/Pages/Auth/registration.jsx
+++ clientside/src/Pages/Auth/registration.jsx
@@ -19,7 +19,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8000/register", {
+      const res = await axios.post("https://er-mart-backend.onrender.com/register", {
         name,
         email,
         password,
