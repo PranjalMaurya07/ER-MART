4dcf9018528a75cf4ff0b08feec5df3fd864ded4
Pranjal Maurya <140903582+PranjalMaurya07@users.noreply.github.com>
2024-11-19 04:28:20 +0530

    Update useCategory.js

--- clientside/src/hooks/useCategory.js
+++ clientside/src/hooks/useCategory.js
@@ -7,7 +7,7 @@ export default function useCategory() {
     //get cat
     const getCategories = async () => {
       try {
-        const { data } = await axios.get("http://localhost:8000/category");
+        const { data } = await axios.get("https://er-mart-backend.onrender.com/category");
         setCategories(data?.category);
       } catch (error) {
         console.log(error);
