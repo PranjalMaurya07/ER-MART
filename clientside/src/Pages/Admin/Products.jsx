a44fd830dc5763986cdbcfbb03b84ad23ef27da1
Pranjal Maurya <140903582+PranjalMaurya07@users.noreply.github.com>
2024-11-19 04:33:55 +0530

    Update Products.jsx

--- clientside/src/Pages/Admin/Products.jsx
+++ clientside/src/Pages/Admin/Products.jsx
@@ -10,7 +10,7 @@ const Products = () => {
 
   const getAllProducts = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8000/get-products");
+      const { data } = await axios.get("https://er-mart-backend.onrender.com/get-products");
       setProducts(data.products);
     } catch (error) {
       console.log(error);
@@ -39,7 +39,7 @@ const Products = () => {
               >
                 <div className="card m-2" style={{ width: "18rem" }}>
                   <img
-                    src={`http://localhost:8000/get-photo/${p._id}`}
+                    src={`https://er-mart-backend.onrender.com/get-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
